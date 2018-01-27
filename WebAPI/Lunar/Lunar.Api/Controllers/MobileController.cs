8cfee94e368ef4bd125195574b9fd2634b5288f3
Lucas Adorno <emoroy@gmail.com>
2018-01-27 20:56:44 -0200

    [Lunar.Api] Added some key at WebConfig
    Furthermore, i created a method to initialize appconfig values

--- WebAPI/Lunar/Lunar.Api/Controllers/MobileController.cs
+++ WebAPI/Lunar/Lunar.Api/Controllers/MobileController.cs
@@ -1,4 +1,5 @@
 ﻿using Lunar.SharedLibrary.Models;
+using Lunar.SharedLibrary.Utils;
 using MongoDB.Driver;
 using MongoDB.Driver.Builders;
 using Newtonsoft.Json;
@@ -14,12 +15,23 @@ namespace Lunar.Api.Controllers
     [RoutePrefix("api/v1/public")]
     public class MobileController : ApiController
     {
-        private static MongoCollection Collection { get; set; }
+        private static MongoCollection  Collection { get; set; }
+        private static string           MongoAddress;
+        private static string           MongoUser;
+        private static string           MongoPassword;
+        private static string           MongoDatabase;
+        private static string           MongoCollection;
 
         [HttpGet]
         [Route("mobilerecods/{output}")]
         public HttpResponseMessage GetMobileRecordsById(int output)
         {
+            // Initialize AppConfig files
+            if (!InitAppConfigValues())
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, "Failed parsing private configuration values");
+            }
+
             // Initialize mongo properties
             if (!InitMongoDb())
             {
@@ -29,10 +41,10 @@ namespace Lunar.Api.Controllers
             List<string> results = new List<string>();
             try
             {
-                MongoCursor<MobileRecordObject> cursor = Collection.FindAs<MobileRecordObject>(Query.EQ("Output", output));
+                MongoCursor<LunarObject> cursor = Collection.FindAs<LunarObject>(Query.EQ("Output", output));
 
                 // Iterate over all records on collection
-                foreach (MobileRecordObject rec in cursor)
+                foreach (LunarObject rec in cursor)
                 {
                     results.Add(JsonConvert.SerializeObject(rec));
                 }
@@ -45,19 +57,36 @@ namespace Lunar.Api.Controllers
             return Request.CreateResponse(HttpStatusCode.OK, results);
         }
 
+         private static bool InitAppConfigValues()
+        {
+            try
+            {
+                // Mongo Fields
+                MongoUser       = Utils.LoadConfigurationSetting("MongoUser"        , "");
+                MongoPassword   = Utils.LoadConfigurationSetting("MongoPassword"    , "");
+                MongoAddress    = Utils.LoadConfigurationSetting("MongoAddress"     , "");
+                MongoDatabase   = Utils.LoadConfigurationSetting("MongoDatabase"    , "");
+                MongoCollection = Utils.LoadConfigurationSetting("MongoCollection"  , "");
+
+            }
+            catch (Exception ex) { return false; }
+
+            return true;
+        }
+
 
         private static bool InitMongoDb()
         {
             try
             {
                 // Initialize database connection
-                MongoDbContext.Configure("lunar", "lunar", "localhost:27017", true, false, 600000, 600000);
+                MongoDbContext.Configure(MongoUser, MongoPassword, MongoAddress, true, false, 600000, 600000);
 
                 // Get database
-                MongoDatabase mongoDatabase = MongoDbContext.GetDatabase("LunarDb");
+                MongoDatabase mongoDatabase = MongoDbContext.GetDatabase(MongoDatabase);
 
                 // Get collection
-                Collection = mongoDatabase.GetCollection<MobileRecordObject>("TestRecord");
+                Collection = mongoDatabase.GetCollection<MobileRecordObject>(MongoCollection);
 
                 // Sanity Check
                 if (Collection == null)
